1f855fa785c27712bb1cdba34d997e9a055f4297
Jackson J Scollan <js02777@srcs.loc>
2022-01-25 17:42:52 -0600

    Enemies Part1: Complete

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player;
+    public GameEnding gameEnding;
+
     bool m_IsPlayerInRange;
 
     void OnTriggerEnter(Collider other)
@@ -18,7 +20,24 @@ public class Observer : MonoBehaviour
     {
         if (other.transform == player)
         {
-            m_IsPlayerInRange = false;
+            //m_IsPlayerInRange = false;
         }
     }
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+
 }
