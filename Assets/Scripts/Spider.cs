28e3aadaa61773be7fee1793f6ebb3713c7fa761
Jackson J Scollan <js02777@srcs.loc>
2022-02-07 17:50:26 -0600

    Custom John Lemon Game: In progress

---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spider : MonoBehaviour
+{
+    public NavMeshAgent spider;
+    public Transform[] waypoints;
+    public Transform player;
+
+    int m_CurrentWaypointIndex;
+
+    void Update()
+    {
+        spider.SetDestination(player.position);
+    }
+}
