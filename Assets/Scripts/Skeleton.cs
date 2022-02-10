e0849eb390d0a541244e76507048482423d6f934
Jackson J Scollan <js02777@srcs.loc>
2022-02-10 17:47:37 -0600

    John Lemon Custom Game: In progress

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Skeleton : MonoBehaviour 
+{
+
+    public NavMeshAgent enemy;
+    public Transform player;
+    public Transform[] moveWaypoint;
+    int m_CurrentWaypointIndex;
+    public int follow = 9;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
+        if (distance < follow)
+        {
+            enemy.SetDestination(player.position);
+        }
+        else
+        {
+            enemy.SetDestination(moveWaypoint[m_CurrentWaypointIndex].position);
+        }
+    }
+}
